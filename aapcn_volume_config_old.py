ad8edcc9f2ba455fbf7ce974170ea2dc964d0f85
namitakarnatak <35719693+namitakarnatak@users.noreply.github.com>
2018-01-23 13:05:28 +0530

    Update aapcn_volume_config_old.py

--- aapcn_volume_config_old.py
+++ aapcn_volume_config_old.py
@@ -7,5 +7,15 @@ PARABOLE_DA = 1071
 
 whatsapp_monthly_flag_offer_id=[113]
 
+offer_id_check_iser600 = [1383]
+offer_id_check_iser2000 = [1384]
+offer_id_check_iser3000 = [1385]
+offer_id_check_iser6000 = [1386]
+offer_id_check_iser10000 = [1387]
+offer_id_check_iser30000 = [1388]
+offer_id_check_iser80000 = [1389]
+offer_id_check_iser120000 = [1390]
+
+
 mymeg10_flag_check = [107]
 mymeg50_flag_check = [108]
