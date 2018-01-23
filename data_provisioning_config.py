026f560f6569d24c1299a825db3e61abec5c6162
namitakarnatak <35719693+namitakarnatak@users.noreply.github.com>
2018-01-23 12:31:23 +0530

    Add files via upload

---
+++ data_provisioning_config.py
@@ -0,0 +1,16 @@
+queueName= 'core_data_provision'
+
+#home = '/home/Ram/code/Madagascar/appussd/data_provisioning/src'
+
+product_name_iser600 = 'I-Ser@600'
+product_name_iser2000 = 'I-Ser@2000'
+product_name_iser3000 = 'I-Ser@3000'
+product_name_iser6000 = 'I-Ser@6000'
+product_name_iser10000 = 'I-Ser@10000'
+product_name_iser30000 = 'I-Ser@30000'
+product_name_iser80000 = 'I-Ser@80000'
+product_name_iser120000 = 'I-Ser@120000'
+
+workers = 200
+
+bonus = True
