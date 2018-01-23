ac5477d2f1d017e0e209dc1c81d4775751ec6d35
namitakarnatak <35719693+namitakarnatak@users.noreply.github.com>
2018-01-23 13:24:15 +0530

    Update dataprovisioning_config_old.py

--- dataprovisioning_config_old.py
+++ dataprovisioning_config_old.py
@@ -2,5 +2,13 @@ queueName= 'core_data_provision'
 #home = '/home/Ram/code/Madagascar/appussd/data_provisioning/src/core'
 home = '/home/Ram/code/Madagascar/appussd/data_provisioning/src'
 workers = 200
+product_name_iser600 = 'I-Ser@600'
+product_name_iser2000 = 'I-Ser@2000'
+product_name_iser3000 = 'I-Ser@3000'
+product_name_iser6000 = 'I-Ser@6000'
+product_name_iser10000 = 'I-Ser@10000'
+product_name_iser30000 = 'I-Ser@30000'
+product_name_iser80000 = 'I-Ser@80000'
+product_name_iser120000 = 'I-Ser@120000'
 
 bonus = True
